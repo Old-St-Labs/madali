58af88996c9178a59f4b18867a742fb9ce469502
arbengers <arben@old.st>
2024-10-15 21:16:15 +0800

    fix: removing webapp ecs instance for the meantime due to overhead costs (unhealthy healthcheck)

--- terraform/dev/17_webapp/01_ecr/main.tf
+++ terraform/dev/17_webapp/01_ecr/main.tf
@@ -64,59 +64,54 @@ variable "project" {
 }
 
 # //ECR
-module "service-ecr" {
-  source          = "../../../modules/aws_ecr"
-  repository_name = "${var.project}-${var.environment}-${var.service_name}"
-}
+# module "service-ecr" {
+#  source          = "../../../modules/aws_ecr"
+#  repository_name = "${var.project}-${var.environment}-${var.service_name}"
+# }
 # //ECR
 
-resource "null_resource" "docker_image" {
-  depends_on = [ module.service-ecr ]
-  provisioner "local-exec" {
-   command = "docker login ${module.service-ecr.token_proxy_endpoint} -u AWS -p ${module.service-ecr.token_password} >> output.txt && docker info >> output.txt && docker pull alpine >> output.txt && docker tag alpine ${module.service-ecr.repository_url}:latest >> output.txt && docker push ${module.service-ecr.repository_url}:latest >> output.txt"
-  }
-}
+# resource "null_resource" "docker_image" {
+#  depends_on = [ module.service-ecr ]
+#  provisioner "local-exec" {
+#   command = "docker login ${module.service-ecr.token_proxy_endpoint} -u AWS -p ${module.service-ecr.token_password} >> output.txt && docker info >> output.txt && docker pull alpine >> output.txt && docker tag alpine ${module.service-ecr.repository_url}:latest >> output.txt && docker push ${module.service-ecr.repository_url}:latest >> output.txt"
+#  }
+# }
 
 
 
 # ECS Task Definition
-module "webapp_task_definition" {
-  source = "../../../modules/aws_ecs_task_definition"
+# module "webapp_task_definition" {
+#  source = "../../../modules/aws_ecs_task_definition"
 
-  task_definition_name = "${var.project}-${var.environment}-${var.service_name}-task"
-  task_role_arn        = var.ecs_task_role
-  execution_role_arn   = var.ecs_task_role
-  ecr_repository_url   = module.service-ecr.repository_url
-  container_port       = 3000
-  host_port            = 3000
-  cpu                  = "256"
-  memory               = "512"
-  log_group            = "${var.project}-${var.environment}-${var.service_name}-log-group"
-  log_region           = var.region
-  os_family            = "LINUX"
-  cpu_architecture     = "X86_64"
-
-}
+#  task_definition_name = "${var.project}-${var.environment}-${var.service_name}-task"
+#  task_role_arn        = var.ecs_task_role
+#  execution_role_arn   = var.ecs_task_role
+#  ecr_repository_url   = module.service-ecr.repository_url
+#  container_port       = 3000
+#  host_port            = 3000
+#  cpu                  = "256"
+#  memory               = "512"
+#  log_group            = "${var.project}-${var.environment}-${var.service_name}-log-group"
+#  log_region           = var.region
+#  os_family            = "LINUX"
+#  cpu_architecture     = "X86_64"
+# }
 
 ## ECS Cluster
-module "ecs_cluster" {
-  source = "../../../modules/aws_ecs_cluster"
+# module "ecs_cluster" {
+#  source = "../../../modules/aws_ecs_cluster"
+#  cluster_name = "${var.project}-${var.environment}-${var.service_name}-cluster"
+# }
 
-  cluster_name = "${var.project}-${var.environment}-${var.service_name}-cluster"
-}
-
-module "webapp_ecs_service" {
-  source = "../../../modules/aws_ecs"
-  service_name     = "${var.project}-${var.environment}-${var.service_name}"
-  cluster          = module.ecs_cluster.ecs_cluster_id
-  task_definition  = module.webapp_task_definition.ecs_task_definition_arn
-  desired_count    = 1
-  subnets          = var.private_subnet_ids
-  security_groups  = var.alb_security_group
-  target_group_arn = var.target_group_arn
-  container_name   = "${var.project}-${var.environment}-${var.service_name}-task"
-
-  container_port   = 3000
-
- 
- }
+# module "webapp_ecs_service" {
+#  source = "../../../modules/aws_ecs"
+#  service_name     = "${var.project}-${var.environment}-${var.service_name}"
+#  cluster          = module.ecs_cluster.ecs_cluster_id
+#  task_definition  = module.webapp_task_definition.ecs_task_definition_arn
+#  desired_count    = 1
+#  subnets          = var.private_subnet_ids
+#  security_groups  = var.alb_security_group
+#  target_group_arn = var.target_group_arn
+#  container_name   = "${var.project}-${var.environment}-${var.service_name}-task"
+#  container_port   = 3000
+# }
